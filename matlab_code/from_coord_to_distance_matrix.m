32d65c7b50d08e810c626cc315f65817b3fb7d1d
Carlo De Simone <111244326+CarloDeSimone@users.noreply.github.com>
2022-08-18 12:45:27 +0200

    Update from_coord_to_distance_matrix.m

--- matlab_code/from_coord_to_distance_matrix.m
+++ matlab_code/from_coord_to_distance_matrix.m
@@ -6,7 +6,7 @@ function dist_M = from_coord_to_distance_matrix(coordinates_M,n)
 dist_M = zeros(n,n);       %initialization of the distance matrix
 for i = 1:n                %for every column (i) of dist_M, i = 1,...,n
     %the column (i) of (dist_M) is filled with the euclidean distances between every node and node (i)
-    dist_M(:,i) = sqrt(sum((coordinates_M-coordinates_M(i,:)).^2,2)); 
+    dist_M(:,i) = sqrt(sum((coordinates_M-coordinates_M(i,:)).^2,2));
 end
 
 end
